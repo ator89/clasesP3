bc4d6bb46fc12a74608fb99c3ee2e4f6622faaf6
Angel Torres <ator89@outlook.com>
2018-10-15 09:28:09 -0600

    imprimir secuencia

--- main.cpp
+++ main.cpp
@@ -1,11 +1,23 @@
-//
+//leer un número e imprimir la secuencia hasta ese num.
 
-#include <iostream>
+#include <iostream> //directiva de preprocesador con la librería iostream
 using std::cout;
 using std::endl;
+using std::cin;
 
 int main(){
 
-   cout << "Hola Mundo, desde VIM y Ubuntu Linux"<<endl; 
+   int num;
+   cout << "Hola Mundo, desde VIM y Ubuntu Linux"<< endl;
+
+   cout << "Ingrese un número: "; cin >> num;
+
+   if(num <0){
+      cout << "Ingrese enteros positivos";
+   }else{
+      for(int i = 1; i<=num; i++){
+         cout << i << endl;
+      }
+   }
    return 0;
 }
