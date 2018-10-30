9d4e09744a7f1133fccaeb9ce0abd4eeb93674e7
Angel Torres <ator89@outlook.com>
2018-10-30 09:11:07 -0600

    Clase 181030

---
+++ semana3/Laberinto.h
@@ -0,0 +1,32 @@
+#ifndef LABERINTO_H //directiva de preprocesador if indefinido
+#define LABERINTO_H 
+
+class Laberinto{
+    private:
+        char** charMatrix;
+        int size;
+    public:
+        //Constructor debe tener el mismo nombre de la clase
+        Laberinto();
+
+        //métodos
+        //libera la memoria
+        void liberarMatriz();//no hay parametros porque ya está encapsulad
+        //crea la matriz en memoria
+        void provisionarMatriz();
+        //leer laberinto del archivo
+        void cargarLaberinto(char*);
+        
+        //imprimir matriz
+        void printMatrix();
+        //procesa recursivamente las casillas
+        bool solve(int,int);  //se necesita ver la casilla procesada
+        
+        //Destructor
+        ~Laberinto();
+        
+        
+
+};
+
+#endif
